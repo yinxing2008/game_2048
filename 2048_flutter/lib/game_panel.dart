7c76c427325babfab12b3a1f722fed5aa673b190
jerry <309032663@qq.com>
2023-01-06 21:01:25 +0800

    update

--- 2048_flutter/lib/game_panel.dart
+++ 2048_flutter/lib/game_panel.dart
@@ -125,29 +125,28 @@ class GamePanelState extends State<GamePanel> {
   _panelOnUpdate(DragUpdateDetails details, Offset lastPosition) {
     final currentPosition = details.globalPosition;
 
-    /// 首先区分是垂直方向还是水平方向滑动
-    if ((currentPosition.dx - lastPosition.dx).abs() > _mainAxisMinLimit &&
-        (currentPosition.dy - lastPosition.dy).abs() < _crossAxisMaxLimit) {
-      // 水平方向滑动
-      if (!_firstValidPan) {
-        return;
-      }
-      if (currentPosition.dx - lastPosition.dx > 0) {
-        _moveRight();
-      } else if (currentPosition.dx - lastPosition.dx < 0) {
-        _moveLeft();
-      }
-      _firstValidPan = false;
-    } else if ((currentPosition.dy - lastPosition.dy).abs() > _mainAxisMinLimit &&
+    if ((currentPosition.dy - lastPosition.dy).abs() > _mainAxisMinLimit &&
         (currentPosition.dx - lastPosition.dx).abs() < _crossAxisMaxLimit) {
       // 垂直方向滑动
       if (!_firstValidPan) {
         return;
       }
-      if (currentPosition.dy - lastPosition.dy > 0) {
-        _moveDown();
-      } else if (currentPosition.dy - lastPosition.dy < 0) {
+      if (currentPosition.dy - lastPosition.dy < 0) {
         _moveUp();
+      } else if (currentPosition.dy - lastPosition.dy > 0) {
+        _moveDown();
+      }
+      _firstValidPan = false;
+    } else if ((currentPosition.dx - lastPosition.dx).abs() > _mainAxisMinLimit &&
+        (currentPosition.dy - lastPosition.dy).abs() < _crossAxisMaxLimit) {
+      // 水平方向滑动
+      if (!_firstValidPan) {
+        return;
+      }
+      if (currentPosition.dx - lastPosition.dx < 0) {
+        _moveLeft();
+      } else if (currentPosition.dx - lastPosition.dx > 0) {
+        _moveRight();
       }
       _firstValidPan = false;
     }
